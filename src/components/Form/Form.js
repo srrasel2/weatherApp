55711adf6bd0cba4311acad6748b2fff0edf3419
Rajib karmaker <rajibchandrakarmaker@gmail.com>
2018-09-02 17:28:12 +0600

    UX Design
    UX Design

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,6 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 
 class Form extends Component {
   constructor(props) {
@@ -12,44 +13,59 @@ class Form extends Component {
     this.state = {};
   }
 
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(44);
+  };
+
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.container}>
-        <FormControl className={classes.margin}>
-          <InputLabel
-            FormLabelClasses={{
-              root: classes.cssLabel,
-              focused: classes.cssFocused
-            }}
-            htmlFor="custom-css-input"
+        <form onSubmit={this.handleSubmit}>
+          <FormControl className={classes.margin}>
+            <InputLabel
+              FormLabelClasses={{
+                root: classes.cssLabel,
+                focused: classes.cssFocused
+              }}
+              htmlFor="custom-css-input"
+            >
+              City
+            </InputLabel>
+            <Input
+              classes={{
+                underline: classes.cssUnderline
+              }}
+              id="custom-css-input"
+            />
+          </FormControl>
+          <FormControl className={classes.margin}>
+            <InputLabel
+              FormLabelClasses={{
+                root: classes.cssLabel,
+                focused: classes.cssFocused
+              }}
+              htmlFor="custom-css-input"
+            >
+              Country
+            </InputLabel>
+            <Input
+              classes={{
+                underline: classes.cssUnderline
+              }}
+              id="custom-css-input"
+            />
+          </FormControl>
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+            type="submit"
           >
-            City
-          </InputLabel>
-          <Input
-            classes={{
-              underline: classes.cssUnderline
-            }}
-            id="custom-css-input"
-          />
-        </FormControl>
-        <FormControl className={classes.margin}>
-          <InputLabel
-            FormLabelClasses={{
-              root: classes.cssLabel,
-              focused: classes.cssFocused
-            }}
-            htmlFor="custom-css-input"
-          >
-            Country
-          </InputLabel>
-          <Input
-            classes={{
-              underline: classes.cssUnderline
-            }}
-            id="custom-css-input"
-          />
-        </FormControl>
+            Submit
+          </Button>
+        </form>
       </div>
     );
   }
---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from './styles';
+
+const Title = () => {
+  return (
+    <Header className="header">
+      <h2>Weather App</h2>
+    </Header>
+  );
+};
+
+export default Title;
