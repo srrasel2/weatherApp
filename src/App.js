a17d244e870211a926ef2d57aa6b545f89ebf4ac
Rajib karmaker <rajibchandrakarmaker@gmail.com>
2018-09-02 12:59:16 +0600

    Configured files & dev dependency
    Configured files, material and styled NPM

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/Form/Form';
+import { Para } from './styles';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Para />
+        <Form />
       </div>
     );
   }
